f2ea30aba0970b251d062a890e190c34dda6566b
FryderykG <eszkimos@gmail.com>
2017-03-30 13:43:58 +0200

    add state to tha App and changeDeadline method change state on input and button click

--- countdownChamp/src/App.js
+++ countdownChamp/src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline});
+  }
+
   render() {
     return (
       <div className="app">
-        <div className="app-title">Countdown to December 25, 2017</div>
+        <div className="app-title">
+          Countdown to {this.state.deadline}
+        </div>
         <div>
           <div className="clock days">14 days</div>
           <div className="clock hours">30 hours</div>
@@ -13,8 +27,11 @@ class App extends Component {
           <div className="clock seconds">20 seconds</div>
         </div>
         <div>
-          <input placeholder="new date" />
-          <button>Submit</button>
+          <input
+            placeholder="new date"
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
+          <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
     )
