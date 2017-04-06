60908245ebe3b51a86e02aa229559f7370afb234
FryderykG <eszkimos@gmail.com>
2017-04-06 11:56:23 +0200

    add App stylesheet add bootstrap components to App

--- music_master/src/App.js
+++ music_master/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return(
-      <div>
+      <div className="App">
         <div className="App-title">Music master</div>
-        <div>
-          <input placeholder="search artist..."/>
-          <button>Search</button>
-        </div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl type="text" placeholder="search artist..."/>
+            <InputGroup.Addon>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
         <div className="Profile">
           <div>Artist Picture</div>
           <div>Artist Name</div>
