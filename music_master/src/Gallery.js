bfceebddb2950dd38adc0946d0581ee555a4f64b
FryderykG <eszkimos@gmail.com>
2017-04-09 18:44:38 +0200

    add playing audio feature to gallery component

--- music_master/src/Gallery.js
+++ music_master/src/Gallery.js
@@ -2,15 +2,53 @@ import React, { Component } from 'react';
 import './App.css'
 
 export default class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playingUrl: '',
+      audio: null,
+      playing: false
+    }
+  }
+  playAudio(previewUrl) {
+    let audio = new Audio(previewUrl);
+    if (!this.state.playing) {
+      audio.play();
+      this.setState({
+        playing: true,
+        playingUrl: previewUrl,
+        audio: audio
+      });
+    } else {
+      if (this.state.playingUrl === previewUrl) {
+        this.state.audio.pause();
+        this.setState({
+          playing: false
+        })
+      } else {
+        this.state.audio.pause();
+        audio.play();
+        this.setState({
+          playingUrl: previewUrl,
+          audio: audio
+        });
+      }
+    }
+  }
+
   render() {
-    console.log('gallery props', this.props);
     const { tracks } = this.props;
     return(
       <div>
         {tracks.map((track, index) => {
+          console.log(track);
           const trackImg = track.album.images[0].url;
           return (
-            <div key={index} className="track">
+            <div
+              key={index}
+              className="track"
+              onClick={() => this.playAudio(track.preview_url)}
+            >
               <img src={trackImg} alt="track" className="track-img"/>
               <p className="track-text">
                 {track.name}
