5f823d0c7b96185d9438cdce5088e53af26aa8a9
FryderykG <eszkimos@gmail.com>
2017-04-09 13:48:59 +0200

    add state to App component add search function(take search parameter from input and return json object from spotify API)

--- music_master/src/App.js
+++ music_master/src/App.js
@@ -3,15 +3,42 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    const BASE_URL = "https://api.spotify.com/v1/search?";
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => console.log('json', json))
+  }
+
   render() {
     return(
       <div className="App">
         <div className="App-title">Music master</div>
         <FormGroup>
           <InputGroup>
-            <FormControl type="text" placeholder="search artist..."/>
-            <InputGroup.Addon>
-              <Glyphicon glyph="search"></Glyphicon>
+            <FormControl
+              type="text"
+              placeholder="Search for the Artist"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search" />
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
